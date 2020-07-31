7a2ee25ceec8ff1b5e7ac58914b9d0c2cef05b33
Grace Wang <7.knicksfan.7@gmail.com>
2020-07-31 04:24:06 +0000

    fix user-room db code

--- app.js
+++ app.js
@@ -1,115 +1,135 @@
 // Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
+// const { App } = require("@slack/bolt");
+const AWSdb = require("aws-sdk");
+const db = require(__dirname+"/ddb.js");
 
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  endpoints: {
-     events: '/slack/events',
-     commands: '/slack/command' 
-   }
-});
-let user_id;
+// const app = new App({
+//     token: process.env.SLACK_BOT_TOKEN,
+//     signingSecret: process.env.SLACK_SIGNING_SECRET,
+//     endpoints: {
+//         events: '/slack/events',
+//         commands: '/slack/command'
+//     }
+// });
+let fname = 'Grace';
+let lname = 'Alexander';
+let chatName = 'LP1';
+let slack_user_id;
+let slack_channel_id;
+const docClient = new AWSdb.DynamoDB.DocumentClient();
+const table = "AlexaSkillHackathon";
+db.initdb();
 
+// db.putdb(table, chatName, fname, lname, docClient, function(data){
+//     if (data === null){
+//         console.log('error')
+//     }
+//     else{
+//         console.log(data);
+//     }
+// });
 
-app.event('app_home_opened', async ({ event, context }) => {
-  try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    user_id = event.user;
-    
-    const result = await app.client.views.publish({
+// app.event('app_home_opened', async ({ event, context }) => {
+//     try {
+//         /* view.publish is the method that your app uses to push a view to the Home tab */
+//         slack_user_id = event.user;
 
-      /* retrieves your xoxb token from context */
-      token: context.botToken,
+//         const result = await app.client.views.publish({
 
-      /* the user that opened your app's app home */
-      user_id: event.user,
+//             /* retrieves your xoxb token from context */
+//             token: context.botToken,
 
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
+//             /* the user that opened your app's app home */
+//             user_id: event.user,
 
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to SuperTeam* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "Head on over to the Messages tab and send the /join-room message to join your SuperTeam!"
-            }
-          }
-        ]
-      }
-    });
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
+//             /* the view object that appears in the app home*/
+//             view: {
+//                 type: 'home',
+//                 callback_id: 'home_view',
 
-
-
-app.command('/create-room', async ({ ack, payload, context }) => {
-  console.log(payload);
-  // Calling ack() acknowledges the request coming from Slack
-  ack();
-  
-  try {
-    // Call the conversations.create method using the built-in WebClient
-    const result = await app.client.conversations.create({
-      // The token you used to initialize your app is stored in the `context` object
-      token: context.botToken,
-      // The name of the conversation
-      name: "public-channel",
-      
-      is_private: false,
-      
-      // Add the user who clicked the message action into the new channel 
-      user_ids: payload.user_id
-    });
-
-    // addUserToRoom();
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
+//                 /* body of the view */
+//                 blocks: [
+//                     {
+//                         "type": "section",
+//                         "text": {
+//                             "type": "mrkdwn",
+//                             "text": "*Welcome to SuperTeam* :tada:"
+//                         }
+//                     },
+//                     {
+//                         "type": "divider"
+//                     },
+//                     {
+//                         "type": "section",
+//                         "text": {
+//                             "type": "mrkdwn",
+//                             "text": "Head on over to the Messages tab and send the /join-room message followed by your first and last name to join your SuperTeam! For example: /join-room Amazon Alexa"
+//                         }
+//                     }
+//                 ]
+//             }
+//         });
+//     }
+//     catch (error) {
+//         console.error(error);
+//     }
+// });
 
 
-app.command('/join-room', async ({ ack, payload, context }) => {
-  console.log(user_id);
-  // Calling ack() acknowledges the request coming from Slack
-  ack();
-  
-  try {
-    // Call the conversations.create method using the built-in WebClient
-    const result = await app.client.conversations.invite({
-      // The token you used to initialize your app is stored in the `context` object
-      token: context.botToken,
-      // The name of the conversation
-      channel: "C0180ED3EBX",
-      // Add the user who clicked the message action into the new channel 
-      users: user_id
-    });
 
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
+// app.command('/create-room', async ({ ack, payload, context }) => {
+//     console.log(payload);
+//     // Calling ack() acknowledges the request coming from Slack
+//     ack();
+
+//     try {
+//         // Call the conversations.create method using the built-in WebClient
+//         const result = await app.client.conversations.create({
+//             // The token you used to initialize your app is stored in the `context` object
+//             token: context.botToken,
+//             // The name of the conversation
+//             name: "public-channel",
+
+//             is_private: false,
+
+//             // Add the user who clicked the message action into the new channel
+//             user_ids: payload.user_id
+//         });
+
+//         slack_channel_id = "C0EAQDV4Z";
+
+//         // addUserToRoom();
+//         console.log(result);
+//     }
+//     catch (error) {
+//         console.error(error);
+//     }
+// });
+
+
+// app.command('/join-room', async ({ ack, payload, context }) => {
+//     console.log(slack_user_id);
+
+//     // Calling ack() acknowledges the request coming from Slack
+//     ack();
+
+//     try {
+//         // Call the conversations.create method using the built-in WebClient
+//         const result = await app.client.conversations.invite({
+//             // The token you used to initialize your app is stored in the `context` object
+//             token: context.botToken,
+//             // The name of the conversation
+//             channel: "C0180ED3EBX",
+//             // Add the user who clicked the message action into the new channel
+//             users: slack_user_id
+//         });
+
+//         console.log(result);
+//     }
+//     catch (error) {
+//         console.error(error);
+//     }
+// });
 
 
 
@@ -118,9 +138,10 @@ app.command('/join-room', async ({ ack, payload, context }) => {
 
 
 
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+// (async () => {
+//     // Start your app
+//     await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
-})();
+//     console.log('⚡️ Bolt app is running!');
+// })();
+
--- ddb.js
+++ ddb.js
@@ -13,57 +13,50 @@ function initdb(){
         endpoint: "http://dynamodb.us-west-2.amazonaws.com",
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        sessionToken: process.env.AWS_SESSION_TOKEN  
-      });
-      var dynamodb = new AWSdb.DynamoDB();
-      
-      var params = {
-          TableName : "TestBlogdb",
-          KeySchema: [       
-              { AttributeName: "id", KeyType: "HASH"},  //Partition key
-          ],
-          AttributeDefinitions: [    
-              { AttributeName: "RoomName", AttributeType: "S" },    
-              { AttributeName: "FirstName", AttributeType: "S" },
-              { AttributeName: "LastName", AttributeType: "S" }
-      
-          ],
-          ProvisionedThroughput: {       
-              ReadCapacityUnits: 1, 
-              WriteCapacityUnits: 1
-          }
-      };
-      
-      dynamodb.createTable(params, function(err, data) {
-          if (err) {
-              console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-          } else {
-              console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-          }
-      });
+        sessionToken: process.env.AWS_SESSION_TOKEN
+    });
+    var dynamodb = new AWSdb.DynamoDB();
+
+    var params = {
+        TableName : "AlexaSkillHackathon",
+        KeySchema: [
+            { AttributeName: "FirstName", KeyType: "HASH"},  //Partition key
+        ],
+        AttributeDefinitions: [
+            { AttributeName: "FirstName", AttributeType: "S" },
+            { AttributeName: "LastName", AttributeType: "S" },
+            { AttributeName: "RoomName", AttributeType: "S" },
+        ],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+        }
+    };
+
+    dynamodb.createTable(params, function(err, data) {
+        if (err) {
+            console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+        }
+    });
 }
 
 function querydb(table, docClient, render){
-    var today = new Date();
 
     var params = {
         TableName: table,
-        IndexName: "DateTimeIndex",
-        KeyConditionExpression: "postdate = :date1",
-        ExpressionAttributeValues: {
-          ":date1": today
-        }
-      };
-      docClient.query(params, onquery);
-    
-      function onquery(err, data) {
+    };
+    docClient.query(params, onquery);
+
+    function onquery(err, data) {
         if (err) {
             console.error("Unable to query the table. Error JSON:", JSON.stringify(err, null, 2));
         } else {
             // console.log(data.Items);
             render(data);
         }
-      }
+    }
 }
 
 
@@ -71,25 +64,22 @@ function getdb(table, pid, docClient, render){
     var params = {
         TableName: table,
         Key:{
-            "id": pid
+            "FirstName": pid
         }
     };
 
     docClient.get(params, function(err, data) {
-    if (err) {
-        render(null);
-        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-    } else {
-        render(data);
-        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-    }
+        if (err) {
+            render(null);
+            console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            render(data);
+            console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        }
     });
 }
 
-function putdb(table, id, modified_id, post, title, docClient, render){
-    var postdate = new Date();
-    var posttime = postdate.getValue();
-
+function putdb(table, chatName, fname, lname, docClient, render){
     // if (typeof variable == 'undefined') {
     //     id = uuidv4();
     //     modified_id = id;
@@ -98,13 +88,9 @@ function putdb(table, id, modified_id, post, title, docClient, render){
     var params = {
         TableName:table,
         Item:{
-            "id": id,
-            "modified_id": modified_id,
-            "body": post,
-            "title": title,
-            "postdate": postdate,
-            "posttime": posttime
-
+            "FirstName": fname,
+            "LastName": lname,
+            "RoomName": chatName,
         }
     };
 
@@ -118,6 +104,6 @@ function putdb(table, id, modified_id, post, title, docClient, render){
             render(true);
             console.log("Added item:", JSON.stringify(data, null, 2));
         }
-    
+
     });
 }
